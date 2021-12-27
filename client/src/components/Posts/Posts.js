0eeab51178d7ed1cee260461c586118d85953e75
theSaab <saba.fathi@emailgmail.info>
2021-12-27 11:03:41 -0500

    env

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Grid, CircularProgress } from '@material-ui/core'
-import _ from 'lodash';
 import Post from './Post/Post';
 import useStyles from './styles'
 
