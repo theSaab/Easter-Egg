a86add2ec0c71e69814e7eb0c643843021af2c2f
theSaab <saba.fathi@emailgmail.info>
2021-12-27 11:10:13 -0500

    trying to fix

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
-dotenv.config();
+require("dotenv").config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
